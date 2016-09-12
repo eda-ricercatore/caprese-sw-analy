ee01eae36e46c1c31715447b66f466eb85fe09b7
Zhiyang Ong <ongz@acm.org>
2016-09-11 21:09:22 -0500

    Starting part 2

---
+++ zero-assign/part-2/setup.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#sudo curl -O https://raw.githubusercontent.com/antlr/grammars-v4/master/java/Java.g4
+curl -O https://raw.githubusercontent.com/antlr/grammars-v4/master/java/Java.g4
+
+
